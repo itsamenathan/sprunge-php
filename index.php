ca2209d3b56c246a30cf1fafacde3cf7388d992d
Nathan W <nathan@frcv.net>
2014-07-30 15:03:16 -0500

    added highlightjs.org syntax highlighting and some normal html tags

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ $mysqli = new mysqli($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASS, $MYSQL_DB);
 
 if (empty($_GET) and empty($_POST)) {
 echo <<< EOT
+<!DOCTYPE html>
+<html>
+<head>
 <pre>
 sprunge(1)                          SPRUNGE                          sprunge(1)
 
@@ -15,9 +18,7 @@ SYNOPSIS
     &lt;command&gt; | curl -F 'sprunge=<-' https://frcv.net/pastebin/
 
 DESCRIPTION
-    NOTE: THIS IS NOT WORKING RIGHT NOW!!!
-    add ?&lt;lang&gt; to resulting url for line numbers and syntax highlighting
-    use this form to paste from a browser
+    syntax hightlighting by http://highlightjs.org/
 
 EXAMPLES
     ~$ cat bin/ching | curl -F 'sprunge=<-' https://frcv.net/pastebin/
@@ -26,26 +27,51 @@ EXAMPLES
 SEE ALSO
     http://github.com/rupa/sprunge
 </pre>
+</head>
+<body>
 EOT;
 }
 
-if($_GET['id']){
-  $hash = $mysqli->real_escape_string($_GET['id']);
-  $result = $mysqli->query("SELECT data FROM post where hash = '$hash'");
-  $row = $result->fetch_assoc();
-  printf("<pre>%s</pre>",$row["data"]);
+if ( !empty($_GET) ) {
+  if($_GET['id']){
+    $hash = $mysqli->real_escape_string($_GET['id']);
+    $result = $mysqli->query("SELECT data FROM post where hash = '$hash'");
+    $row = $result->fetch_assoc();
+echo <<< EOT
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    background-color: #f8f8f8;
+}
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.1/styles/github.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.1/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+EOT;
+
+    printf("<pre><code>%s</code></pre>",$row["data"]);
+
+echo <<< EOT
+</head>
+<body>
+EOT;
+  }
 }
 
-if ($_POST['sprunge']){
-  $data = $mysqli->real_escape_string($_POST['sprunge']);
-  $hash = hash("crc32b", $data, false); 
-  $mysqli->real_query("SELECT hash FROM post where hash = $hash");
-  if($mysqli->field_count === 0){
-    $mysqli->real_query("INSERT INTO post (hash,data) VALUES ('$hash', '$data')");
-    echo "https://frcv.net/pastebin/?id=$hash\n";
-  }
-  else{
-    echo "Hmm, there was a problem.  Please try again!";
+if ( !empty($_POST) ) {
+  if($_POST['sprunge']){
+    $data = $mysqli->real_escape_string($_POST['sprunge']);
+    $hash = hash("crc32b", $data, false); 
+    $mysqli->real_query("SELECT hash FROM post where hash = $hash");
+    if($mysqli->field_count === 0){
+      $mysqli->real_query("INSERT INTO post (hash,data) VALUES ('$hash', '$data')");
+      echo "https://frcv.net/pastebin/?id=$hash\n";
+    }
+    else{
+      echo "Hmm, there was a problem.  Please try again!";
+    }
   }
 }
 ?>
