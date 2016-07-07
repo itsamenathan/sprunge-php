ce4895126e937e1fc0273c22bfdf55ecc0c93e0d
Nathan W <nathan@frcv.net>
2016-07-07 00:21:45 -0500

    update highlight.js to 9.5.0

--- id.php
+++ id.php
@@ -6,8 +6,8 @@ body {
     background-color: #f8f8f8;
 }
 </style>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/styles/github.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js"></script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/styles/github.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.5.0/highlight.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
 <pre><code><?=$data;?></code></pre>
 </head>
